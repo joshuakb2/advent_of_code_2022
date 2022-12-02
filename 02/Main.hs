4c58e6b39d322891e49ac74f0f134716fb3d6b13
Joshua Baker <joshuakb2@gmail.com>
2022-12-01 22:48:56 -0700

    Day 2 part 2

--- 02/Main.hs
+++ 02/Main.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE LambdaCase #-}
+
 main = do
     input <- getContents
     let part1 = solvePart1 input
@@ -9,27 +11,59 @@ solvePart1 =
     show
     . sum
     . map (\s ->
-        score (charToRPS (s !! 0)) (charToRPS (s !! 2))
+        score (charToPlay (s !! 0)) (charToPlay (s !! 2))
     )
     . filter (not . null)
     . lines
 
-solvePart2 input = "Not implemented"
+solvePart2 =
+    show
+    . sum
+    . map (\s ->
+        let opponentPlay = charToPlay (s !! 0)
+            desiredOutcome = charToOutcome (s !! 2)
+            yourPlay = playForOutcome desiredOutcome opponentPlay
+        in score opponentPlay yourPlay
+    )
+    . filter (not . null)
+    . lines
 
-charToRPS c
+charToPlay c
     | c == 'A' || c == 'X' = Rock
     | c == 'B' || c == 'Y' = Paper
     | c == 'C' || c == 'Z' = Scissors
-    | otherwise = error "Invalid RPS char"
+    | otherwise = error "Invalid play char"
+
+charToOutcome = \case
+    'X' -> OpponentWins
+    'Y' -> Draw
+    'Z' -> YouWin
+
+playForOutcome :: Outcome -> Play -> Play
+playForOutcome outcome opponent =
+    toEnum ((fromEnum opponent + offset) `mod` 3)
+    where
+        offset =
+            case outcome of
+                YouWin -> 1
+                Draw -> 0
+                OpponentWins -> -1
 
 score opponent you =
-    1 + fromEnum you + outcomeScore
-    where
-        outcomeScore =
-            case (fromEnum you - fromEnum opponent) `mod` 3 of
-                0 -> 3 -- Draw
-                1 -> 6 -- You win
-                2 -> 0 -- Opponent wins
-            
+    playScore you + outcomeScore (outcomeOf opponent you)
 
-data RPS = Rock | Paper | Scissors deriving (Eq, Ord, Enum, Show)
+playScore play = fromEnum play + 1
+
+outcomeOf opponent you =
+    case (fromEnum you - fromEnum opponent) `mod` 3 of
+        0 -> Draw
+        1 -> YouWin
+        2 -> OpponentWins
+
+outcomeScore = \case
+    YouWin -> 6
+    Draw -> 3
+    OpponentWins -> 0
+
+data Outcome = YouWin | Draw | OpponentWins deriving (Eq, Show)
+data Play = Rock | Paper | Scissors deriving (Eq, Ord, Enum, Show)

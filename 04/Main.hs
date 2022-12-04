f1cf265fe408395ab6e68db6c1dcb8a6a58f2ae6
Joshua Baker <joshuakb2@gmail.com>
2022-12-04 02:52:52 -0700

    Simplify day 4 one more time

--- 04/Main.hs
+++ 04/Main.hs
@@ -21,16 +21,15 @@ parseLine :: String -> ((Int, Int), (Int, Int))
 parseLine line = (a, b)
     where
         (aStr, bStr) = splitOn ',' line
-        (a1Str, a2Str) = splitOn '-' aStr
-        (b1Str, b2Str) = splitOn '-' bStr
-        a = (read a1Str, read a2Str)
-        b = (read b1Str, read b2Str)
+        a = (read `both`) (splitOn '-' aStr)
+        b = (read `both`) (splitOn '-' bStr)
 
 splitOn c s = (before, after)
     where
         (before, rest) = span (/= c) s
         after = drop 1 rest
 
-(a1, a2) `covers` (b1, b2) = a1 <= b1 && a2 >= b2
+f `both` (a, b) = (f a, f b)
 
+(a1, a2) `covers` (b1, b2) = a1 <= b1 && a2 >= b2
 (a1, a2) `overlaps` (b1, b2) = a1 <= b2 && a2 >= b1
